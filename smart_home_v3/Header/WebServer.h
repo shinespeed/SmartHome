e7b6477ad3d6f6ea2921e6d8b410a3e2de43d684
Artur <42372037+shinespeed@users.noreply.github.com>
2021-02-12 19:23:38 +0200

    Update WebServer.h

--- smart_home_v3/Header/WebServer.h
+++ smart_home_v3/Header/WebServer.h
@@ -6,15 +6,15 @@
 class WebServer: public Controller
 {
 public:
-	WebServer();
+    WebServer();
 
-	AsyncWebServer server;
-	Controller* _telegram;
+    AsyncWebServer server;
+    Controller* _telegram;
 
-	bool change_mode_bell;
-
-	void init_server();
-	void send_message(const String& text);
-	void set_pointer_telegram(Controller* telegram);
-	void update_message_telegram();
+    bool change_mode_bell;
+    
+   void init_server(); 
+   void send_message(const String& text); 
+   void set_pointer_telegram(Controller* telegram);
+   void update_message_telegram();
 };
