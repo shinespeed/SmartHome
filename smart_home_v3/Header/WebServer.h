2d6adeb8b7a0731fe77765a5e6ccff94871d802c
Artur <42372037+shinespeed@users.noreply.github.com>
2021-02-12 22:04:08 +0200

    Update WebServer.h

--- smart_home_v3/Header/WebServer.h
+++ smart_home_v3/Header/WebServer.h
@@ -13,8 +13,8 @@ public:
 
     bool change_mode_bell;
     
-   void init_server(); 
-   void send_message(const String& text); 
-   void set_pointer_telegram(Controller* telegram);
-   void update_message_telegram();
+    void init_server(); 
+    void send_message(const String& text); 
+    void set_pointer_telegram(Controller* telegram);
+    void update_message_telegram();
 };
