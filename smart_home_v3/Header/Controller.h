05fd803993b46e739d3d08bd6505fd9f9108e5c8
Artur <42372037+shinespeed@users.noreply.github.com>
2021-02-12 19:24:02 +0200

    Update Controller.h

--- smart_home_v3/Header/Controller.h
+++ smart_home_v3/Header/Controller.h
@@ -7,13 +7,13 @@
 class Controller
 {
 public: 
-   Controller();
+    Controller();
    
-   SPIFFS_DATA* _spiffs_data;
+    SPIFFS_DATA* _spiffs_data;
 
-   void switch_bell_state(bool state);
-   void switch_call_relay(bool state);
-   void load_data_SPIFFS();
-   void set_spiffs_data(SPIFFS_DATA* spiffs_data); 
-   virtual void write_message_telegram(String chat_id, String text);
+    void switch_bell_state(bool state);
+    void switch_call_relay(bool state);
+    void load_data_SPIFFS();
+    void set_spiffs_data(SPIFFS_DATA* spiffs_data); 
+    virtual void write_message_telegram(String chat_id, String text);
 };
