8dc2068699394060394410eb511c464333640b79
Artur <42372037+shinespeed@users.noreply.github.com>
2021-02-12 19:19:09 +0200

    Update Calling.h

--- smart_home_v3/Header/Calling.h
+++ smart_home_v3/Header/Calling.h
@@ -6,30 +6,30 @@
 class Telegraph
 {
 public:
-	Telegraph();
+    Telegraph();
 
-	long _last_time;
-	int  _buf_time_telegraph;
-	int  _timer_telegraph;
-	int  _time_dahs;
-	int  _time_dits;
-	bool _telegraph_state;
+    long _last_time; 
+    int  _buf_time_telegraph;
+    int  _timer_telegraph;
+    int  _time_dahs;
+    int  _time_dits;
+    bool _telegraph_state;
 
-	void telegraph_dahs();
-	void telegraph_dits();
-	void telegraph_code_bin(const String& code_bin);
-	void telegraph_bin_ASCII(const char& symbol_ASCII);
+    void telegraph_dahs();
+    void telegraph_dits();
+    void telegraph_code_bin(const String& code_bin);
+    void telegraph_bin_ASCII(const char& symbol_ASCII);
 };
 
 class Call: public Controller
 {
 public:
-	Call();
+    Call();
 
-	long _last_time;
-	long _timer_call;
+    long _last_time;
+    long _timer_call;
 
-	void load_spiffs();
-	void get_call();
+    void load_spiffs();
+    void get_call();
     String set_time_call(String time_millis);
 };
