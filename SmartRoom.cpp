e164a6510f578f67a366d235943941cc9a0ae5fa
Artur <42372037+shinespeed@users.noreply.github.com>
2021-04-10 08:48:55 +0300

    Update SmartRoom.cpp

--- SmartRoom.cpp
+++ SmartRoom.cpp
@@ -2,9 +2,9 @@
 
 
 SmartRoom::SmartRoom(): _spiffs_data(new SPIFFS_DATA()),
-	                    _telegram(new Telegram(_web_server, _sensor, _spiffs_data)),
+	                _telegram(new Telegram(_web_server, _sensor, _spiffs_data)),
                         _web_server(new WebServer(_telegram, _sensor, _spiffs_data)),
-	                    _sensor(new Sensor(_telegram, _web_server, _spiffs_data))
+	                _sensor(new Sensor(_telegram, _web_server, _spiffs_data))
 {}
 
 SmartRoom::~SmartRoom()
