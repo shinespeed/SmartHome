f0dfd73d19f6d3ebf669db4f2ee7a6eff159d8c9
Artur <42372037+shinespeed@users.noreply.github.com>
2021-04-10 08:55:24 +0300

    Update Telegram.cpp

--- Telegram.cpp
+++ Telegram.cpp
@@ -1,1 +1,14 @@
 
+#include "Telegram.h"
+
+
+Telegram::Telegram(Controller* web_server, Controller* sensor, Controller* spiffs_data):
+	_web_server(web_server),
+	_sensor(sensor),
+	_spiffs_data(spiffs_data),
+	_bot(BOT_TOKEN, _client)
+{
+}
+
+Telegram::~Telegram()
+{}
